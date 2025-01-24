40780ea19a0d8286e75739fdfea9b7e8687861f3
Irwan Fario Subastian <irwan@cadmus.io>
2025-01-24 11:10:16 +1100

    Update plugin version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024123000;
+$plugin->version = 2025012400;
 $plugin->requires = 2023100900;
 $plugin->component = 'assignsubmission_ltisubmissions';
 $plugin->incompatible = 500;
