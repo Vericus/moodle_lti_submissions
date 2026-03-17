d58b6e9de6b8b8b481fe2a0c225f3aaa1243e164
Pankaj Kumar Rawat <pankaj@cadmus.io>
2026-03-17 12:12:20 +0530

    chore: bump incompatible Moodle version to 5.2 (#18)
    * chore: bump incompatible Moodle version to 5.13
    
    * docs: update release notes for Moodle 5.13 incompatibility
    
    * Bump version and adjust incompatible value
    
    * Update CHANGELOG incompatibility version
    
    * Include plugin version in LTI launch params
    
    * Add plugin version to custom params
    
    * Use custom_moodle_plugin_version param

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # LTI Submissions Plugin
 
+## 2026-03-16 
+- Update plugin incompatibility version to Moodle 502
+- Adds plugin version to custom params
+
 ## 2025-09-17
 - Make HTTP requests using Moodle's curl class to support proxies [#15](https://github.com/Vericus/moodle_lti_submissions/pull/15)
 
--- lib.php
+++ lib.php
@@ -275,7 +275,12 @@ function assignsubmission_ltisubmissions_get_launch_data($instance, $nonce = '',
 
     $requestparams['launch_presentation_return_url'] = $returnurl;
 
-    // Add the parameters configured by the LTI services.
+    // Add plugin version and the parameters configured by the LTI services.
+    $pluginversion = get_config('assignsubmission_ltisubmissions', 'version');
+    if (!empty($pluginversion)) {
+        $requestparams['custom_moodle_plugin_version'] = (string)$pluginversion;
+    }
+
     if ($typeid && !$islti2) {
         $services = assignsubmission_ltisubmissions_get_services();
         foreach ($services as $service) {
